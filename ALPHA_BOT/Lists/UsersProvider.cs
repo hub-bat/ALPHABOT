488764e49935d66e323e31ca6c8f22483cfe880c
hub-bat <89218060+hub-bat@users.noreply.github.com>
2025-12-21 01:04:26 -0500

    Update UsersProvider.cs

--- ALPHA_BOT/Lists/UsersProvider.cs
+++ ALPHA_BOT/Lists/UsersProvider.cs
@@ -6,6 +6,6 @@ namespace ALPHA_BOT.Lists;
 
 public class UsersProvider : SimpleAutoCompleteProvider
 {
-    static DiscordAutoCompleteChoice[] UserList = [ .. File.ReadAllLines("~/Lists/test.txt").Select(l => l.Split(' ')).Select(p => new DiscordAutoCompleteChoice(p[1], p[0]))];
+    static DiscordAutoCompleteChoice[] UserList = [ .. File.ReadAllLines("test.txt").Select(l => l.Split(' ')).Select(p => new DiscordAutoCompleteChoice(p[1], p[0]))];
     protected override IEnumerable<DiscordAutoCompleteChoice> Choices => UserList;
 }
