a209c6dfee1d43b29852b6ed9ca5e2eb4129bf68
hub-bat <89218060+hub-bat@users.noreply.github.com>
2025-12-21 01:31:42 -0500

    fixing newline delimiter

--- ALPHA_BOT/Lists/UsersProvider.cs
+++ ALPHA_BOT/Lists/UsersProvider.cs
@@ -6,6 +6,6 @@ namespace ALPHA_BOT.Lists;
 
 public class UsersProvider : SimpleAutoCompleteProvider
 {
-    static DiscordAutoCompleteChoice[] UserList = [ .. File.ReadAllLines("user_list.txt").Select(l => l.Split(' ')).Select(p => new DiscordAutoCompleteChoice(p[0], p[0]))];
+    static DiscordAutoCompleteChoice[] UserList = [ .. File.ReadAllLines("user_list.txt").Select(l => l.Split('\n')).Select(p => new DiscordAutoCompleteChoice(p[0], p[0]))];
     protected override IEnumerable<DiscordAutoCompleteChoice> Choices => UserList;
 }
