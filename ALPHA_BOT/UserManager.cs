cf4745e66b3b0ce020ee32b56e182f72b3bae419
hub-bat <89218060+hub-bat@users.noreply.github.com>
2025-12-30 18:31:29 -0500

    Update UserManager.cs

--- ALPHA_BOT/UserManager.cs
+++ ALPHA_BOT/UserManager.cs
@@ -27,17 +27,10 @@ namespace ALPHA_BOT
         
         [Command("kick")]
         [Description("Guardian's Kick - Administrator only")]
+        [RequirePermissions(userPermissions: [DiscordPermission.Administrator], botPermissions: [])]
         public static async ValueTask KickAsync(CommandContext context, [SlashAutoCompleteProvider<UsersProvider>][Description("Username to kick.")] string user, [Description("Amount of time.")] string time, [Description("Reason for kick.")] string reason)
         {
-            DiscordMember userEx = context.Member;
-            if (userEx.Permissions == DiscordPermission.Administrator)
-            {
                 await context.RespondAsync($"**GUARDIAN has KICKED {user} for {time} | REASON: {reason}**");
-            }
-            //else
-            //{
-            //    await context.RespondAsync($"YOU DO NOT HAVE PERMISSION.");
-            //}
         }
     }
 }
